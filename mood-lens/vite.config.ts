b4c24f0e930a2dd0fbe39cd54611413bd9ff2ec7
ad0v0 <olha.vadiasova@gmail.com>
2026-02-24 17:59:03 +0200

    [Mood Lens] Add base to vite config

--- mood-lens/vite.config.ts
+++ mood-lens/vite.config.ts
@@ -8,6 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/dev-lab/mood-lens/",
   server: {
     open: true,
     port: 2000,
